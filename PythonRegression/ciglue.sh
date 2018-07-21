f7ec3e6558db149affe11077c7303c83d0a0572c
Andrea Villa <andreakarimodm@gmail.com>
2018-07-21 14:52:53 +0200

    Functional ciglue.sh

--- PythonRegression/ciglue.sh
+++ PythonRegression/ciglue.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-git clone --depth 1 --branch yaml_config https://github.com/karimodm/iri-network-tests.git iri-network-tests
+ERROR=0
+
+git clone --depth 1 --branch yaml_configuration https://github.com/karimodm/iri-network-tests.git iri-network-tests
 cd iri-network-tests
 virtualenv venv
 source venv/bin/activate
@@ -10,13 +12,28 @@ pip install -r requirements.txt
 
 cd ..
 for machine_dir in tests/features/machine?; do
-  python iri-network-tests/create_cluster.py -d -c $machine_dir/cluster.yml -o $machine_dir/output.yml
+  python iri-network-tests/create_cluster.py -d -c $machine_dir/config.yml -o $machine_dir/output.yml
+  if [ $? -ne 0 ]; then
+    ERROR=1
+    python <<EOF
+import yaml
+for (key,value) in yaml.load(open('$machine_dir/output.yml'))['nodes'].iteritems():
+  if value['status'] == 'Error':
+    print value['log']
+EOF
+  fi
 done
 REVISION_HASH=$(grep -F 'revision_hash:' $machine_dir/output.yml | cut -d' ' -f2)
 deactivate
 
-###
-RUN ALOE TESTS HERE
-###
+if [ $ERROR -eq 0 ]; then
 
-iri-network-tests/teardown_cluster.sh -r $REVISION_HASH
+  echo "Starting tests..."
+  ###
+  #RUN ALOE TESTS HERE
+  ###
+
+fi
+
+timeout 10 iri-network-tests/teardown_cluster.sh -r $REVISION_HASH
+exit $ERROR
